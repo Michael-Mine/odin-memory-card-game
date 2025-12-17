6282401d492db78c11a98855f428a87ab241e0e5
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2025-12-17 08:42:00 +0000

    refactor: move handle click to Game component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,43 +1,18 @@
 import "../styles/cards.css";
 
-function shuffleList(list) {
-  const newList = list.sort(() => Math.random() - 0.5);
-  return newList;
-}
-
-function ListItem({ item, list, setList, currentCards, setCurrentCards }) {
-  const handleButtonClick = () => {
-    if (!currentCards.includes(item.id)) {
-      const addID = [...currentCards, item.id];
-      console.log(addID);
-      setCurrentCards(addID);
-    } else {
-      setCurrentCards([]);
-    }
-    setList(shuffleList(list));
-  };
-
+function ListItem({ item, handleClick }) {
   return (
-    <button onClick={handleButtonClick}>
+    <button onClick={() => handleClick(item.id)}>
       <img src={item.image} alt="" className="cards-item" />
     </button>
   );
 }
 
-export function CardList({ list, setList, currentCards, setCurrentCards }) {
+export function CardList({ list, handleClick }) {
   return (
     <div className="cards-container">
       {list.map((item) => {
-        return (
-          <ListItem
-            key={item.id}
-            item={item}
-            list={list}
-            setList={setList}
-            currentCards={currentCards}
-            setCurrentCards={setCurrentCards}
-          />
-        );
+        return <ListItem key={item.id} item={item} handleClick={handleClick} />;
       })}
     </div>
   );
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 import { useCardImageURL } from "./Data";
 import { CardList } from "./Cards";
 
+function shuffleList(list) {
+  const newList = list.sort(() => Math.random() - 0.5);
+  return newList;
+}
+
 export function Game() {
   const { cardImageURLs, setcardImageURLs, error, loading } = useCardImageURL();
   const [currentCardsClicked, setCurrentCardsClicked] = useState([]);
@@ -20,16 +25,22 @@ export function Game() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>A network error was encountered</p>;
 
+  const handleButtonClick = (itemId) => {
+    if (!currentCardsClicked.includes(itemId)) {
+      const addID = [...currentCardsClicked, itemId];
+      console.log(addID);
+      setCurrentCardsClicked(addID);
+    } else {
+      setCurrentCardsClicked([]);
+    }
+    setcardImageURLs(shuffleList(cardImageURLs));
+  };
+
   return (
     <div>
       <p>Current Score: {currentScore}</p>
       <p>Best Score: {bestScore}</p>
-      <CardList
-        list={cardImageURLs}
-        setList={setcardImageURLs}
-        currentCards={currentCardsClicked}
-        setCurrentCards={setCurrentCardsClicked}
-      />
+      <CardList list={cardImageURLs} handleClick={handleButtonClick} />
     </div>
   );
 }
